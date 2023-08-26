e55dbc8c86dcd42e86c961ee5658ed0a70482bd9
benjamin <benjamin.choron@outlook.com>
2023-08-26 17:49:24 +0200

    feat(i18n): add internationalization

---
+++ src/@types/resources.ts
@@ -0,0 +1,9 @@
+import enDictionary from '../i18n/en/translation.json';
+import frDictionary from '../i18n/fr/translation.json';
+
+const resources = {
+  enDictionary,
+  frDictionary,
+} as const;
+
+export default resources;
--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useTranslation, Trans } from 'react-i18next';
+import './i18n/config.ts';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { t } = useTranslation();
+
+  const count = 3;
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>My React Template</h1>
+      <p>{t('title', { name: 'John' })}</p>
+      <p>{t('description.part1')}</p>
+      <p>{t('description.part2')}</p>
+      <Trans i18nKey="userMessagesUnread" count={count}>
+        You have {{ count }} unread message.
+      </Trans>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
