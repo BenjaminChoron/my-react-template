2a81fdf2c11390db550480504c050a03b5ede12e
benjamin <benjamin.choron@outlook.com>
2023-09-01 15:29:06 +0200

    refactor: better use of react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,12 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-import './App.css';
-
-import Home from './pages/Home';
-import Page from './pages/Page';
-import PageNotFound from './pages/PageNotFound';
+import { Outlet } from 'react-router-dom';
 import Nav from './components/Nav/Nav';
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <Nav />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/page" element={<Page />} />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-    </BrowserRouter>
+      <Outlet />
+    </>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
-
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './i18n/config.ts';
+import './index.css';
+import App from './App.tsx';
+import Home from './pages/Home.tsx';
+import Page from './pages/Page.tsx';
+import PageNotFound from './pages/PageNotFound.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/page',
+        element: <Page />,
+      },
+      {
+        path: '*',
+        element: <PageNotFound />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
